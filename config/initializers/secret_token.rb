8404b224c14910c70f70daae8ea86d5ce2460d40
Priyanta Dharmasena <pdharmasena@salesforce.com>
2011-10-08 20:08:31 -0700

    Adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My1stDemo::Application.config.secret_token = 'c2886715e46ed0c153487688a100f17ca7b1abc7e0245a9dea37fdb3495b31970c1c813fdc7abada5b4e87896a703db057995c253055594380790c9e580e149a'
